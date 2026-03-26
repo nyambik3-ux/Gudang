a8ee7a2da9d1718a3eae0277b7bc881ff875ab37
nyambik3-ux <nyambik3@gmail.com>
2026-03-26 19:10:50 +0700

    Create layout.js

---
+++ src/app/layout.js
@@ -0,0 +1,12 @@
+export const metadata = {
+  title: 'Gudang System',
+  description: 'Sistem Manajemen Gudang',
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="id">
+      <body>{children}</body>
+    </html>
+  )
+}
